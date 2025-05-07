8c806e58ee75e2ce32f3bbb564f2fe67375eae2c
majvit <edvin@majvillantransport.com>
2025-05-07 09:28:48 +0200

    Added main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router-dom";
 import App from "./App";
 import Record from "./components/Record";
@@ -10,40 +10,40 @@ import RecordList from "./components/RecordList";
 import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
+    {
         path: "/",
-        element: <RecordList />,
-      },
-    ],
-  },
-  {
-    path: "/edit/:id",
-    element: <App />,
-    children: [
-      {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <RecordList />,
+            },
+        ],
+    },
+    {
         path: "/edit/:id",
-        element: <Record />,
-      },
-    ],
-  },
-  {
-    path: "/create",
-    element: <App />,
-    children: [
-      {
+        element: <App />,
+        children: [
+            {
+                path: "/edit/:id",
+                element: <Record />,
+            },
+        ],
+    },
+    {
         path: "/create",
-        element: <Record />,
-      },
-    ],
-  },
+        element: <App />,
+        children: [
+            {
+                path: "/create",
+                element: <Record />,
+            },
+        ],
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
